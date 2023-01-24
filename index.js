74a895a38b3929ba4060473a0bf018ea9d211975
pfornari <95132472+pfornari@users.noreply.github.com>
2023-01-23 23:56:45 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+export { default as AddItem } from './add-item';
+export { default as TaskList } from './task/index';
+export { default as CustomModal } from './modal/index';
+export { default as CustomCheckBox } from './checkbox/index';
+export { default as TaskIndex } from './task/item/index';
